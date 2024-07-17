58b7593bc8204c68d3f67da3872a51d098855c08
SeungWoo <wjknn3123@gmail.com>
2024-07-17 15:35:23 +0900

    아이콘 및 로고 컴포넌트화

--- src/assets/index.tsx
+++
@@ -1,5 +0,0 @@
-import Left from "./Img/Arrow_Left.png";
-
-export const Images = {
-  Left,
-};
--- src/page/logIn/page.tsx
+++ src/page/logIn/page.tsx
@@ -1,5 +1,5 @@
-import { Outlet } from "react-router-dom";
-import LogInCompo from "../../components/organism/logIn";
+import { Outlet } from 'react-router-dom';
+import LogInCompo from '../../components/organism/logIn';
 
 export default function LogInPage() {
   return (
